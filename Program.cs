99da1f8edbb0c85caff6955bc88d6135829ef9e5
user <user@DESKTOP-FIAVR8I>
2022-06-13 04:19:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Take a number as input from the user
+//Display the word Tick if the number is even, and Tock if the number is odd
+Console.Write("Please Enter a number from 1 to 12: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int tickTock = number % 2;
+if (number > 12)
+    Console.Write("Did you enter a number between 1 and 12? Try again");
+
+else if (tickTock == 0)
+        Console.WriteLine("Tick");
+   else
+       Console.WriteLine("Tock");
+
+
+
